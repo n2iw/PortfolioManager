0326d517e54a2c5b146cc8ae8e44b0de03e62dff
n2iw <yingming18@gmail.com>
2015-06-01 21:59:30 -0400

    Add validations to model picture and adminuser

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,6 +5,9 @@ class Picture < ActiveRecord::Base
   has_attached_file :file, :styles => { large: "800x800>", thumb: "200x200>" }
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 
+  validates :file, presence: true
+  validates :work_id, presence: true
+
   scope :sorted, lambda { order('position') }
 
 end
