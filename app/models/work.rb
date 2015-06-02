3d408e64a6c9dee5f63306587926f3ec96148608
n2iw <yingming18@gmail.com>
2015-06-01 21:52:39 -0400

    Add validations on work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,11 +1,18 @@
 class Work < ActiveRecord::Base
-  before_save :pre_process
+  before_validation :pre_process
 
   has_many :pictures, dependent: :destroy
   acts_as_list
 
   has_attached_file :thumbnail, :styles => { :thumb => "200x200>" }
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :thumbnail, presence: true
+                    
+
+
 
   scope :visible, lambda { where(visible: true) }
   scope :sorted, lambda { order('position') }
