fd037df034906cc3e801321a1d37bec75f4b99bc
n2iw <yingming18@gmail.com>
2015-07-01 12:39:20 -0400

    Refactor Capistrano setting files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,32 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+set :rails_env, :production
+#set :stage, :production
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{ubuntu@52.1.250.117}
+role :web, %w{ubuntu@52.1.250.117}
+role :db,  %w{ubuntu@52.1.250.117}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+#server '52.1.250.117', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
+
+set :application, 'moyuzhang_app'
+set :repo_url, 'git@bitbucket.org:jamesying/moyuzhang_app.git'
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -20,6 +46,24 @@ set :linked_files, %w{config/secrets.yml db/production.sqlite3}
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+server '52.1.250.117',
+  user: 'ubuntu',
+  roles: %w{web app db},
+  ssh_options: {
+    keys: %w(/Users/james/Mydoc/id/james-key-useast.pem)
+    # password: 'please use keys'
+  }
+# setting per server overrides global ssh_options
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
